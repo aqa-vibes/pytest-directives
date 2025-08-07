bd5942bd71c01abaf214419a65484bd7ce041626
odinykt <kaktusaprel@gmail.com>
2025-08-07 12:19:14 +0300

    add mypy checks

--- tasks.py
+++ tasks.py
@@ -7,6 +7,11 @@ def tests(context: Context):
     context.run("uv run pytest tests/ --ignore=tests/pytest_directives/test_data/")
 
 @task
-def check(context: Context):
+def linter(context: Context):
     """Run ruff checks."""
     context.run("uv run ruff check .")
+
+@task
+def types(context: Context):
+    """Run mypy checks."""
+    context.run("uv run mypy")
